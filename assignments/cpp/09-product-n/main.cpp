0350f107eaffdd4bba8e905908346bd7e8b7d4ea
100049461-cyber <100049461@csisd.org>
2026-09-10 22:04:14 +0000

    Submit: cpp/09-product-n

--- assignments/cpp/09-product-n/main.cpp
+++ assignments/cpp/09-product-n/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     // Write your solution here.
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {   
+        cin >> v[i];
+    }
+    long long answer = 1;
+    for (int i = 0; i < n; i++) {
+        answer = (answer * v[i]) % 1000000007LL;
+    }
+    cout << answer << "\n";
     return 0;
 }
