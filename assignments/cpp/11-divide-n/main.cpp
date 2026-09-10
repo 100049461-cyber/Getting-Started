3af41e83f38218d9948574a9dc7a8583210fdc38
100049461-cyber <100049461@csisd.org>
2026-09-10 22:06:09 +0000

    Submit: cpp/11-divide-n

--- assignments/cpp/11-divide-n/main.cpp
+++ assignments/cpp/11-divide-n/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     // Write your solution here.
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    int repdiv = v[0];
+    for (int i = 1; i < n; i++) {
+        repdiv = repdiv / v[i];
+    }
+    cout << repdiv << "\n";
     return 0;
 }
