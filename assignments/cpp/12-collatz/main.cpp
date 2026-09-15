02bca8771c5763a9ce178875287537991c37e60f
100049461-cyber <100049461@csisd.org>
2026-09-15 21:31:51 +0000

    Submit: cpp/12-collatz

--- assignments/cpp/12-collatz/main.cpp
+++ assignments/cpp/12-collatz/main.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 int main() {
-    // Write your solution here.
+    long long n = 0;
+    cin >> n;
+    while (n != 1) {
+        cout << n << " ";
+        if (n % 2 == 1) {
+            n = 3*n + 1;
+        }
+        else if (n % 2 == 0) {
+            n = n / 2;
+        }
+    }
+    cout << 1;
     return 0;
 }
