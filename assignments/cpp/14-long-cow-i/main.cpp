cf01479a41851006be147cd78a2194fff9fa19fa
100049461-cyber <100049461@csisd.org>
2026-09-15 21:55:22 +0000

    Submit: cpp/14-long-cow-i

--- assignments/cpp/14-long-cow-i/main.cpp
+++ assignments/cpp/14-long-cow-i/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // Write your solution here.
+    string dih;
+    getline(cin, dih);
+    int d = dih.length();
+    getline(cin, dih);
+    int ih = dih.length();
+    if (d > ih) {
+        cout << "nohj\n";
+    } else if (d < ih) {
+        cout << "john\n";
+    } else {
+        cout << "-1\n";
+    }
     return 0;
 }
