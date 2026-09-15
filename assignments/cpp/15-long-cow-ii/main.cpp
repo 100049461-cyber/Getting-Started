8507f0dc18d9269eb79dc4e605459956482da898
100049461-cyber <100049461@csisd.org>
2026-09-15 21:59:13 +0000

    Submit: cpp/15-long-cow-ii

--- assignments/cpp/15-long-cow-ii/main.cpp
+++ assignments/cpp/15-long-cow-ii/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // Write your solution here.
+    string dih;
+    string dihl;
+    int mx = 0;
+    int n;
+    cin >> n;
+    cin.ignore();
+    for (int i = 0; i < n; i++) {
+        getline(cin, dih);
+        if (dih.length() > mx) {
+            mx = dih.length();
+            dihl = dih;
+        }
+    }
+    cout << mx << "\n" << dihl << "\n";
     return 0;
 }
